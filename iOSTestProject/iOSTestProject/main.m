801f5ae9abfd64eaa5cb7b107361173746defd46
ilfocus <qiwang0711@gmail.com>
2019-10-18 09:10:14 +0800

    __attribute__测试

--- iOSTestProject/iOSTestProject/main.m
+++ iOSTestProject/iOSTestProject/main.m
@@ -9,9 +9,22 @@
 #import <UIKit/UIKit.h>
 #import "AppDelegate.h"
 
+__attribute__((constructor(101)))
+void beforeFuntion101() {
+	NSLog(@"Before function101");
+}
+__attribute__((constructor(102)))
+void beforeFuntion102() {
+	NSLog(@"Before function102");
+}
+__attribute__((constructor(103)))
+void beforeFuntion103() {
+	NSLog(@"Before function103");
+}
 int main(int argc, char * argv[]) {
 	NSString * appDelegateClassName;
 	@autoreleasepool {
+		NSLog(@"main function");
 	    // Setup code that might create autoreleased objects goes here.
 	    appDelegateClassName = NSStringFromClass([AppDelegate class]);
 	}
